988741a8f29cdee1d162f0705c39d1141594f445
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-06-09 11:48:13 +0100

    Finished Navbar and made it mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+	Footer,
+	Blog,
+	Possibility,
+	Features,
+	Header,
+	WhatGPT3,
+} from "./containers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<Navbar />
+				<Header />
+			</div>
+			<Brand />
+			<WhatGPT3 />
+			<Features />
+			<Possibility />
+			<CTA />
+			<Blog />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
