d750f1edace46062fe752f6177f5a588fd16aced
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-06-12 14:19:39 +0100

    Finished possibility section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Menu = () => (
 			<a href="#home">Home</a>
 		</p>
 		<p>
-			<a href="#wgpt3">What is GPT3?</a>
+			<a href="#whatGPT3">What is GPT3?</a>
 		</p>
 		<p>
 			<a href="#possibility">Open AI</a>
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './possibility.css'
+import React from "react";
+import possibilityImage from "../../assets/possibility.png";
+import "./possibility.css";
 
 const Possibility = () => {
-  return (
-    <div>Possibility</div>
-  )
-}
+	return (
+		<div
+			className="gpt3__possibility section__padding"
+			id="possibility">
+			<div className="gpt3__possibility-image">
+				<img
+					src={possibilityImage}
+					alt="possibility"
+				/>
+			</div>
+			<div className="gpt3__possibility-content">
+				<h4>Request early access to get started</h4>
+				<h1 className="gradient__text">
+					The possibilities are beyong your imagination
+				</h1>
+				<p>
+					Yet bed any for travelling assistance indulgence unpleasing. Not thoughts
+					all exercise blessing. Indulgence way everything joy alteration boisterous
+					the attachment. Party we years to order allow asked of.
+				</p>
+				<h4>Request for early access to get started</h4>
+			</div>
+		</div>
+	);
+};
 
-export default Possibility
+export default Possibility;
