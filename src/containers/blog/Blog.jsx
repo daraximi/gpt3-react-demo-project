3b85403af188ee95b36b9b952170421bbd1a2d9a
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-06-13 09:26:17 +0100

    Finished Blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Article } from "../../components";
 import "./blog.css";
+import { blog1, blog2, blog3, blog4, blog5 } from "./imports";
 const Blog = () => {
-	return <div>Blog</div>;
+	return (
+		<div
+			className="gpt3__blog section__padding"
+			id="blog">
+			<div
+				className="gpt3__blog-heading"
+				id="blog-heading">
+				<h1 className="gradient__text">
+					A lot is happening. We are blogging about it
+				</h1>
+			</div>
+			<div className="gpt3__blog-container">
+				<div
+					className="gpt3__blog-container_groupA"
+					id="__blog-container_groupA">
+					<Article
+						imgUrl={blog1}
+						date="Sep 22, 2023"
+						title="GPT-3 and Open AI is the future. Let us explore how it is?"
+					/>
+				</div>
+				<div
+					className="gpt3__blog-container_groupB"
+					id="__blog-container_groupB">
+					<Article
+						imgUrl={blog2}
+						date="Sep 22, 2023"
+						title="GPT-3 and Open AI is the future. Let us explore how it is?"
+					/>
+					<Article
+						imgUrl={blog3}
+						date="Sep 22, 2023"
+						title="GPT-3 and Open AI is the future. Let us explore how it is?"
+					/>
+					<Article
+						imgUrl={blog4}
+						date="Sep 22, 2023"
+						title="GPT-3 and Open AI is the future. Let us explore how it is?"
+					/>
+					<Article
+						imgUrl={blog5}
+						date="Sep 22, 2023"
+						title="GPT-3 and Open AI is the future. Let us explore how it is?"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Blog;
