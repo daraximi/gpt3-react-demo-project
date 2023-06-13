a92a1b638ad6e1f11b6360983baaba6a28b020dc
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-06-13 16:49:36 +0100

    finished review

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
 	return (
-		<div className="App">
+		<div className="App"> 
 			<div className="gradient__bg">
 				<Navbar />
 				<Header />
